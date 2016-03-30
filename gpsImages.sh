af9a0ccc5412287cde56636b2385fa1ced0c7bf9
Student <student@MMA-VM>
2016-03-30 16:29:57 +0200

    Converted GPS coordinatates to relative GPS positions

--- gpsImages.sh
+++ gpsImages.sh
@@ -1,11 +1,25 @@
-for f in database/*
+# clear positionDB
+echo "" > positionDB.txt 
 
+# loop trough the database
+for f in database/*
 do
+ # get EXIF GPS data
  out=$(exiftool $f | grep "GPS Position" | cut -d':' -f2)
+ 
+ # if it has a GPS position
  if  [[ $out ]]; then
-    echo $f has GPS $out
-    echo $f has GPS >> log.txt
-    echo $f $out >> positionDB.txt
+    # commented out
+    #echo $f $out >> positionDB.txt
+    
+    # print the converted value
+    pos=$(python convDBLine.py "$f$out")
+    echo $pos
+    echo $pos >> positionDB.txt
+    
+    # and rdo the conversion for the logs
+    #python convDBLine.py "$f$out" >> positionDB.txt
+    
  else 
     rm $f
     echo $f removed
